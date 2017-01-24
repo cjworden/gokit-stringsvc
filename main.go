785d46d25e7948025c403f96915d198ee4c88262
Cody Worden <cjworden@greatergood.com>
2017-01-24 11:01:11 -0800

    Requests and responses

--- main.go
+++ main.go
@@ -28,3 +28,22 @@ func (stringService) Count(s string) int {
 }
 
 var ErrEmpty = errors.New("Empty string")
+
+// Every method in the iterface is modeled as a RPC. We define request & response structs for each method,
+//	capturing all input/output parameters respectively.
+type uppercaseRequest struct {
+	S string `json:"s"`
+}
+
+type uppercaseResponse struct {
+	V string `json:"v"`
+	Err string `json:"err,omitempty"` // errors don't JSON-marshal, so we use a string
+}
+
+type countRequest struct {
+	S string `json:"s"`
+}
+
+type countResponse struct {
+	V int `json:"v"`
+}
