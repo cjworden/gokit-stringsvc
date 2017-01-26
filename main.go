335d2e9c549abb841c18528fa4afade83e4b0fd1
Cody Worden <cjworden@greatergood.com>
2017-01-26 12:45:05 -0800

    Endpoints

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"errors"
 	"strings"
+	"github.com/go-kit/kit/endpoint"
+	"golang.org/x/net/context"
 )
 
 func main() {
@@ -29,14 +31,14 @@ func (stringService) Count(s string) int {
 
 var ErrEmpty = errors.New("Empty string")
 
-// Every method in the iterface is modeled as a RPC. We define request & response structs for each method,
+// Every method in the interface is modeled as a RPC. We define request & response structs for each method,
 //	capturing all input/output parameters respectively.
 type uppercaseRequest struct {
 	S string `json:"s"`
 }
 
 type uppercaseResponse struct {
-	V string `json:"v"`
+	V   string `json:"v"`
 	Err string `json:"err,omitempty"` // errors don't JSON-marshal, so we use a string
 }
 
@@ -46,4 +48,18 @@ type countRequest struct {
 
 type countResponse struct {
 	V int `json:"v"`
+}
+
+// Go kit provides it's functionality through endpoints: type Endpoint func(ctx context.Context, request interface{}) (response interface{}, err error)
+// An endpoint represents a single RPC, or single method of our service interface. We write adapters to convert our
+// service methods into endpoints.
+func makeUppercaseEndpoint(svc StringService) endpoint.Endpoint {
+	return func (ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(uppercaseRequest)
+		v, err := svc.Uppercase(req.S)
+		if err != nil {
+			return uppercaseResponse{v, err.Error()}, nil
+		}
+		return uppercaseResponse{v, " "}, nil
+	}
 }
